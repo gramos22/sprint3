b22f39cd2b4eff756081c7137908adfe584de3b7
gramos22 <90844401+gramos22@users.noreply.github.com>
2022-06-12 18:02:30 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,437 @@
+
+
+
+let data = {
+    headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+    },
+    initialDate: '2022-06-12',
+    navLinks: true, // can click day/week names to navigate views
+    businessHours: true, // display business hours
+    editable: true,
+    selectable: true,
+    events: [{
+        title: '2022-1_AED1_E10',
+        start: '2022-05-29',
+        end: '2022-05-30',
+        color: '#a83e7a'
+    }, {
+        title: '2022-1_AED1_T01',
+        start: '2022-05-30',
+        end: '2022-05-31',
+        color: '#a83e7a'
+    }, {
+        title: 'QUIZ 4.1 - Arquitetura da Web',
+        start: '2022-06-05',
+        end: '2022-06-06',
+        color: '#257e4a'
+    }, {
+        title: 'QUIZ 4.2 - Protocolo HTTP',
+        start: '2022-06-12',
+        end: '2022-06-13',
+        color: '#257e4a'
+    }, {
+        title: '2022-1_AED1_E13',
+        start: '2022-06-14',
+        end: '2022-06-15',
+        color: '#a83e7a'
+    }, {
+        title: 'Prova de AEDS',
+        start: '2022-06-03',
+        end: '2022-06-04',
+        color: '#a83e7a'
+    }, {
+        title: 'Prova de DIW',
+        start: '2022-06-09',
+        end: '2022-06-10',
+        color: '#257e4a'
+    }, {
+        title: 'Estudar Calculo',
+        start: '2022-06-14',
+        end: '2022-06-16',
+        color: '#35b175'
+    }, {
+        title: 'Aniversario',
+        start: '2022-06-20',
+        end: '2022-06-21'
+    }, {
+        title: 'Sprint 4 - Apresentação',
+        start: '2022-06-06',
+        end: '2022-06-07',
+        color: '#c53a47'
+    }, {
+        title: 'Sprint 4 - Documentação do Projeto',
+        start: '2022-06-06',
+        end: '2022-06-07',
+        color: '#c53a47'
+    }, {
+        title: 'Aula de ingles',
+        start: '2022-06-11',
+        end: '2022-06-12',
+        color: '#2f9293'
+    }, {
+        title: 'Aula de ingles',
+        start: '2022-06-18',
+        end: '2022-06-19',
+        color: '#2f9293'
+    }, {
+        title: 'Reavaliação Calculo',
+        start: '2022-06-30',
+        end: '2022-06-31',
+        color: '#35b175'
+    }, {
+        title: 'Campeonato Basquete',
+        start: '2022-06-24',
+        end: '2022-06-25',
+        color: '#b5632d'
+    }, {
+        title: '2022-1_AED1_E13',
+        start: '2022-04-14',
+        end: '2022-04-15',
+        color: '#a83e7a'
+    }, {
+        title: 'Prova de AEDS',
+        start: '2022-04-03',
+        end: '2022-04-04',
+        color: '#a83e7a'
+    }, {
+        title: 'Prova de DIW',
+        start: '2022-05-09',
+        end: '2022-05-10',
+        color: '#257e4a'
+    }, {
+        title: 'Estudar Calculo',
+        start: '2022-05-14',
+        end: '2022-05-16',
+        color: '#35b175'
+    }, {
+        title: 'Aniversario',
+        start: '2022-05-20',
+        end: '2022-05-21'
+    }, {
+        title: 'QUIZ 4.1 - Arquitetura da Web',
+        start: '2022-07-05',
+        end: '2022-07-06',
+        color: '#257e4a'
+    }, {
+        title: 'QUIZ 4.2 - Protocolo HTTP',
+        start: '2022-07-12',
+        end: '2022-07-13',
+        color: '#257e4a'
+    }, {
+        title: '2022-1_AED1_E13',
+        start: '2022-07-14',
+        end: '2022-07-15',
+        color: '#a83e7a'
+    }, {
+        title: 'Prova de AEDS',
+        start: '2022-07-03',
+        end: '2022-07-04',
+        color: '#a83e7a'
+    }, {
+        title: 'Prova de DIW',
+        start: '2022-07-09',
+        end: '2022-07-10',
+        color: '#257e4a'
+    }, {
+        title: 'Estudar Calculo',
+        start: '2022-07-14',
+        end: '2022-07-16',
+        color: '#35b175'
+    }, {
+        title: 'Aniversario',
+        start: '2022-07-20',
+        end: '2022-07-21'
+    }, {
+        title: 'Sprint 4 - Apresentação',
+        start: '2022-07-06',
+        end: '2022-07-07',
+        color: '#c53a47'
+    }, {
+        title: 'Sprint 4 - Documentação do Projeto',
+        start: '2022-07-06',
+        end: '2022-07-07',
+        color: '#c53a47'
+    }, {
+        title: 'Aula de ingles',
+        start: '2022-07-11',
+        end: '2022-07-12',
+        color: '#2f9293'
+    }, {
+        title: 'Aula de ingles',
+        start: '2022-07-18',
+        end: '2022-07-19',
+        color: '#2f9293'
+    }, {
+        title: 'Reavaliação Calculo',
+        start: '2022-07-30',
+        end: '2022-07-31',
+        color: '#35b175'
+    }, {
+        title: 'Campeonato Basquete',
+        start: '2022-07-24',
+        end: '2022-07-25',
+        color: '#b5632d'
+    }, {
+        title: '2022-1_AED1_E13',
+        start: '2022-05-03',
+        end: '2022-05-04',
+        color: '#a83e7a'
+    }, {
+        title: 'Prova de AEDS',
+        start: '2022-05-05',
+        end: '2022-05-06',
+        color: '#a83e7a'
+    }, {
+        title: 'Prova de DIW',
+        start: '2022-05-05',
+        end: '2022-05-06',
+        color: '#257e4a'
+    }, {
+        title: 'Estudar Calculo',
+        start: '2022-05-10',
+        end: '2022-05-11',
+        color: '#35b175'
+    }, {
+        title: 'Aniversario',
+        start: '2022-05-13',
+        end: '2022-05-14'
+    }, {
+        title: '2022-1_AED1_E10',
+        start: '2022-05-14',
+        end: '2022-05-16',
+        color: '#a83e7a'
+    }, {
+        title: '2022-1_AED1_T01',
+        start: '2022-05-15',
+        end: '2022-05-16',
+        color: '#a83e7a'
+    }, {
+        title: 'QUIZ 4.1 - Arquitetura da Web',
+        start: '2022-05-01',
+        end: '2022-05-02',
+        color: '#257e4a'
+    }, {
+        title: 'QUIZ 4.2 - Protocolo HTTP',
+        start: '2022-05-02',
+        end: '2022-05-03',
+        color: '#257e4a'
+    },
+
+    // areas where "Meeting" must be dropped
+    {
+        groupId: 'availableForMeeting',
+        start: '2022-06-11T10:00:00',
+        end: '2022-06-11T16:00:00',
+        display: 'background'
+    }, {
+        groupId: 'availableForMeeting',
+        start: '2022-06-13T10:00:00',
+        end: '2022-06-13T16:00:00',
+        display: 'background'
+    },
+
+        // red areas where no events can be dropped
+        //{
+        //    start: '2022-06-24',
+        //    end: '2022-06-28',
+        //    overlap: false,
+        //    display: 'background',
+        //    color: '#efddef'
+        //}, {
+        //    start: '2022-06-06',
+        //    end: '2022-06-08',
+        //    overlap: false,
+        //    display: 'background',
+        //    color: '#efddef'
+        //}
+    ]
+}
+
+window.onload = () => {
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, data);
+
+    calendar.render();
+});
+
+// Menu de adição de notas
+
+// Init
+
+function init() {
+    // Adiciona funções para tratar os eventos 
+        
+    $("#btnInsert").click(function () {
+        // Verfica se o formulário está preenchido corretamente
+        if (!$("#form-postit")[0].checkValidity()) {
+            //displayMessage("Preencha o formulário corretamente.");
+            return;
+        }
+
+        // Obtem os valores dos campos do formulário
+        let campoDescricao = $("#inputDescricao").val();
+        let campoUrgencia = $("#inputUrgencia").val();
+        let campoDia = $("#inputDia").val();
+        let campoHorario = $("#inputHorario").val();
+        let campoCategoria = $("#inputCategoria").val();
+        let postit = {
+            descricao: campoDescricao,
+            urgencia: campoUrgencia,
+            dia: campoDia,
+            horario: campoHorario,
+            categoria: campoCategoria,
+        };
+
+        insertPostit(postit);
+
+        // Limpa o formulario
+        $("#form-postit")[0].reset();
+    });
+
+    // Intercepta o click do botão Alterar
+    $("#btnUpdate").click(function () {
+        // Obtem os valores dos campos do formulário
+        let campoId = $("#inputId").val();
+        if (campoId == "") {
+            //displayMessage("Selecione um post-it para ser alterado.");
+            return;
+        }
+        let campoDescricao = $("#inputDescricao").val();
+        let campoUrgencia = $("#inputUrgencia").val();
+        let campoDia = $("#inputDia").val();
+        let campoHorario = $("#inputHorario").val();
+        let campoCategoria = $("#inputCategoria").val();
+        let postit = {
+            descricao: campoDescricao,
+            urgencia: campoUrgencia,
+            dia: campoDia,
+            horario: campoHorario,
+            categoria: campoCategoria,
+        };
+
+        updatepostit(parseInt(campoId), postit);
+
+        // Limpa o formulario
+        $("#form-postit")[0].reset();
+    });
+
+    // Intercepta o click do botão Excluir
+    $("#btnDelete").click(function () {
+        let campoId = $("#inputId").val();
+        if (campoId == "") {
+            //displayMessage("Selecione um post-it a ser excluído.");
+            return;
+        }
+        deletepostit(parseInt(campoId));
+
+        // Limpa o formulario
+        $("#form-postit")[0].reset();
+    });
+
+    // Intercepta o click do botão Cancelar
+    
+}
+
+
+// Dados iniciais
+var db_postits_inicial = {
+    "data": [
+        {
+            "id": 1,
+            "descricao": "Sua descrição aqui",
+            "urgencia": "Selecione um nível de urgência",
+            "dia": "15/05/2022",
+            "horario": "10:30",
+            "categoria": "Selecione uma categoria",
+        }
+    ]
+}
+
+var db = JSON.parse(localStorage.getItem('db_postit'));
+if (!db) {
+    db = db_postits_inicial
+};
+
+
+function addNotes() {
+
+    let noteMenu = document.querySelector('.container')
+    noteMenu.style.display = 'block';
+
+    $('#btnCancel').click(function () {
+        noteMenu.style.display = 'none';
+
+        // Limpa o formulario
+        $("#form-postit")[0].reset();
+    });
+
+    init();
+
+    
+}
+
+function insertPostit(postit) {
+    // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
+    let novoId = 1;
+    if (db.data.length != 0)
+        novoId = db.data[db.data.length - 1].id + 1;
+    let novoPostit = {
+        "id": novoId,
+        "descricao": postit.descricao,
+        "urgencia": postit.urgencia,
+        "dia": postit.dia,
+        "horario": postit.horario,
+        "categoria": postit.categoria
+    };
+
+    // Insere o novo objeto no array
+    db.data.push(novoPostit);
+    //displayMessage("Post-it criado com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_postit', JSON.stringify(db));
+}
+
+function updatepostit(id, postit) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = db.data.map(obj => obj.id).indexOf(id);
+
+    // Altera os dados do objeto no array
+    db.data[index].descricao = postit.descricao,
+        db.data[index].dia = postit.dia,
+        db.data[index].horario = postit.horario,
+        db.data[index].urgencia = postit.urgencia,
+        db.data[index].categoria = postit.categoria
+
+    //displayMessage("Post-it alterado com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_postit', JSON.stringify(db));
+}
+
+function deletepostit(id) {
+    // Filtra o array removendo o elemento com o id passado
+    db.data = db.data.filter(function (element) { return element.id != id });
+
+    //displayMessage("Post-it removido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_postit', JSON.stringify(db));
+}
